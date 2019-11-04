27e1356c260e4d37de3a48c6d0f9f698ef358636
chiayhon <571617842@qq.com>
2019-11-05 05:51:06 +0800

    Support for concurrency

--- src/chat/ChatServer.java
+++ src/chat/ChatServer.java
@@ -3,6 +3,7 @@ package chat;
 import java.io.DataInputStream;
 import java.io.EOFException;
 import java.io.IOException;
+import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,52 +14,89 @@ import java.net.Socket;
 public class ChatServer {
 
     public static void main(String[] args) {
+        new ChatServer().startHandle();
+    }
 
+    public void startHandle(){
         boolean started = false;
 
         ServerSocket server = null;
 
+
+        try {
+            server = new ServerSocket(8888);
+            started = true;
+        } catch (BindException e1){
+            System.out.println("端口已被使用,打开服务器失败");
+            System.exit(0);
+        } catch (Exception e2) {
+            System.out.println("绑定端口时出现异常");
+            e2.printStackTrace();
+        }
+
+        try{
+            //监听控制
+            while (started){
+                Socket socket = server.accept();
+                System.out.println("一个客户端连接成功");
+
+                ClientThread client = new ClientThread(socket);
+                new Thread(client).start();
+            }
+        } catch (Exception e) {
+            System.out.println("接受客户端Socket时出现异常");
+            e.printStackTrace();
+        } finally {
+            try {
+                server.close();
+                System.out.println("服务端已关闭");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class ClientThread implements Runnable {
         Socket socket = null;
 
         DataInputStream dis = null;
 
-        try {
-            server = new ServerSocket(8888);
-        } catch (Exception e) {
-            e.printStackTrace();
+        boolean connected = false;
+
+        public ClientThread(Socket socket) {
+            try {
+                this.socket = socket;
+                dis = new DataInputStream(socket.getInputStream());
+                connected = true;
+            } catch (Exception e) {
+                System.out.println("客户端线程初始化失败");
+                e.printStackTrace();
+            }
         }
 
-        try{
-            started = true;
+        @Override
+        public void run() {
+            String str = null;
 
-            //监听控制
-            while (started) {
-                boolean connected = false;
-
-                socket = server.accept();
-System.out.println("一个客户端连接成功");
-                 dis = new DataInputStream(socket.getInputStream());
-                connected = true;
-
-                //数据读取控制
-                while (connected) {
-                    String str = dis.readUTF();
+            try {
+                while (connected){
+                    str = dis.readUTF();
                     System.out.println(str);
                 }
+            } catch (EOFException e1) {
+                System.out.println("客户端已退出");
+            } catch (Exception e2) {
+                System.out.println("客户端线程运行时出现异常");
+                e2.printStackTrace();
+            } finally {
+                try {
+                    if(dis != null )    dis.close();//未被初始化则无需关闭
+                    if(socket != null )    socket.close();
+                }catch (IOException e){
+                    System.out.println("客户端线程关闭时出现异常");
+                    e.printStackTrace();
+                }
             }
-        } catch (EOFException e1){
-            //此异常不处理
-        } catch (Exception e) {
-System.out.println("传输出现异常");
-            e.printStackTrace();
-        } finally {
-            try {
-                if(dis != null )    dis.close();//未被初始化则无需关闭
-                if(socket != null )    socket.close();
-            }catch (IOException e1){
-                e1.printStackTrace();
-            }
-System.out.println("客户端已退出");
         }
     }
 }
