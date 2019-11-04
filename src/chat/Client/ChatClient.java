dfce37a5c3fccd7b35b8eaf9c659709b0ca2200c
chiayhon <571617842@qq.com>
2019-11-04 16:45:18 +0800

    Listener of TextField

--- src/chat/Client/ChatClient.java
+++ src/chat/Client/ChatClient.java
@@ -1,6 +1,8 @@
 package chat.Client;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -10,18 +12,18 @@ import java.awt.event.WindowEvent;
  */
 public class ChatClient extends Frame {
 
-    TextField inputArea = new TextField();
-    TextArea outputArea = new TextArea();
+    TextField inputText = new TextField();
+    TextArea outputText = new TextArea();
 
     public static void main(String[] args) {
         new ChatClient().launchFrame();
     }
 
     public void launchFrame(){
-        this.setLocation(300 , 400);
-        this.setSize(500 , 600);
-        add(inputArea , BorderLayout.SOUTH);
-        add(outputArea , BorderLayout.NORTH);
+        setLocation(300 , 400);
+        setSize(500 , 600);
+        add(inputText , BorderLayout.SOUTH);
+        add(outputText , BorderLayout.NORTH);
         pack();
         this.addWindowListener(new WindowAdapter() {
             @Override
@@ -30,6 +32,21 @@ public class ChatClient extends Frame {
 //                System.exit(0);//关闭虚拟机
             }
         });
+        inputText.addActionListener(new TextFieldAdapter());
         this.setVisible(true);
+    }
+
+    private void addWindowListener(TextFieldAdapter textFieldAdapter) {
+    }
+
+    private class TextFieldAdapter implements ActionListener {
+
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String s = inputText.getText();
+            outputText.setText(s);
+            inputText.setText("");
+        }
     }
 }
