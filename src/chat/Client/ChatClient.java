8eed5791bf5658b4bcc463b2da4d690000068bb3
chiayhon <571617842@qq.com>
2019-11-04 16:13:04 +0800

    TextField and TextArea

--- src/chat/Client/ChatClient.java
+++ src/chat/Client/ChatClient.java
@@ -1,6 +1,8 @@
 package chat.Client;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * @author Chiayhon
@@ -21,6 +23,13 @@ public class ChatClient extends Frame {
         add(inputArea , BorderLayout.SOUTH);
         add(outputArea , BorderLayout.NORTH);
         pack();
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                dispose();//关闭当前窗口组件
+//                System.exit(0);//关闭虚拟机
+            }
+        });
         this.setVisible(true);
     }
 }
